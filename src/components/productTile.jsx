269c73906ff6d98ae678f2cf497f22e644f9bd5a
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-01-21 21:16:13 +0530

    msg

--- src/components/productTile.jsx
+++ src/components/productTile.jsx
@@ -6,19 +6,19 @@ export default function ProductTile({ id, data }) {
   
   
   return (
-    <div className="min-w-96 min-h-96 bg-slate-700 border py-5">
-      <div className="flex w-full h-full bg-slate-700 mx-auto  items-center gap-10 justify-center">
-        <div className=" min-h-[200px] min-w-[400px]  flex justify-start gap-2  pr-20 ">
+    <div className="min-w-96 min-h-96 bg-slate-700 border py-5 w-full">
+      <div className="flex w-full h-full bg-slate-700 mx-auto  items-center sm:gap-10 justify-center">
+        <div className=" min-h-[200px] w-full flex gap-2 justify-center align-middle flex-wrap ">
           {
             data.images.map((img, index) => {
-              return (<div key={index}>                <img src={img} key={index} className="w-[250px] h-[230px] "/>
+              return (<div key={index} className="w-full">                <img src={img} key={index} className="w-[250px]  h-[230px] text-center"/>
               </div>
 
               )
             })
           }
         </div>
-        <div className="border max-w-1/2 max-h-full bg-blue-950 p-5">
+        <div className="border w-full max-h-full bg-blue-950 p-5">
           <p className="text-3xl">
             $ {data.price}
             <span className="line-through ml-5">
@@ -47,7 +47,7 @@ export default function ProductTile({ id, data }) {
         </div>
       </div>
       <button
-        className=" bg-yellow-600  disabled:opacity-50"
+        className=" bg-yellow-600 mt-10 disabled:opacity-50"
         onClick={() => {
           HandleAddToCart(data);
         }}
